8c2bcccefc89901adf96c288c2eceefefbd5856f
Tawaliou ALAO <developtawaliou@gmail.com>
2024-10-30 21:04:03 +0100

    update script file to match assignment2

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -50,13 +50,16 @@ then
 		exit 1
 	fi
 fi
-#echo "Removing the old writer utility and compiling as a native application"
-#make clean
-#make
+
+# Clean any previous build artifacts.
+make clean 
+
+# Compile your writer application using native compilation
+make
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
